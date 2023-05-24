aee0d71db1303b7d265f4ff9d81bfb4d9ec77b05
liancu-uni <liancu@unicode.org>
2023-05-24 10:52:06 -0700

    UCDXML 15.1 Draft 1 (Beta)

--- src/org/unicode/ucd/Version.java
+++ src/org/unicode/ucd/Version.java
@@ -1,6 +1,6 @@
 // COPYRIGHT AND PERMISSION NOTICE
 //
-// Copyright 2006-2022 Unicode Inc.
+// Copyright 2006-2023 Unicode Inc.
 //
 // All rights reserved.
 //
@@ -117,4 +117,5 @@ public final class Version {
   public static final Version V13_0_0 = new Version (13, 0, 0);
   public static final Version V14_0_0 = new Version (14, 0, 0);
   public static final Version V15_0_0 = new Version (15, 0, 0);
+  public static final Version V15_1_0 = new Version (15, 1, 0);
 }
